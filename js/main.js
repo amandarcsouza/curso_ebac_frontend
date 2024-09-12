e598f2c47ef03b363aebe7e5638af85fd9454745
amandarcsouza <amandaroso@yahoo.com>
2024-09-12 15:24:26 -0300

    Delete js/main.js
    Código feito no index.html na tag script

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#telefone').mask('(00) 00000-0000');
-    $('form').validate({        
-        submitHandler: function() {
-            alert("Mensagem enviada com sucesso!")
-        }        
-    });
-})
